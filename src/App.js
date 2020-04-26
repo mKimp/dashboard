d05ab2a09e20d4063a4804eeddfd3ca4a1162561
Kim Ma <pkm@pdx.edu>
2020-04-26 14:41:33 -0700

    add api call

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {render} from 'react-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            parks: [],
+            isLoaded: false
+        }
+    }
+
+    async componentDidMount() {
+
+      try{
+        const response = await fetch('https://developer.nps.gov/api/v1/parks?stateCode=ca&api_key=ggkV9uIryYjb4jyp0qeVrwCwE5rObHy68Il8hhKD')
+        const json = await response.json();
+        this.setState({isLoaded: true, parks: json})
+      } catch (error){
+          console.log(error);
+      }
+    }
+
+    render() { 
+        if (this.state.isLoaded == true) {
+          //  var lat  = this.state.parks.data[0].longtitude;
+          //  console.log(lat);
+            //console.log(this.state.parks.data[0].latLong);
+        } else {
+            console.log("NOTHING");
+        }
+        return (
+            <div className="App">
+                <h1>National Park DashBoard</h1>
+                <input className="state" type="text"></input>
+
+  
+            </div>
+
+        );
+    }
 }
 
 export default App;
