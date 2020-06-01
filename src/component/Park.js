629f2b234a96c9257bbf91b4c2f66ba0cbf3f78b
Kim Ma <pkm@pdx.edu>
2020-05-31 23:21:38 -0700

    fix the variable set up in heroku

--- src/component/Park.js
+++ src/component/Park.js
@@ -22,16 +22,17 @@ class Park extends React.Component {
     }
  
     componentDidMount() {
+        const proxy = "https://stormy-woodland-25848.herokuapp.com/"
         const key = "&api_key=" + process.env.REACT_APP_WEATHER_API_KEY
         const park_key = "&stateCode=&q=&api_key=" + process.env.REACT_APP_WEATHER_API_KEY
         let url = "https://developer.nps.gov/api/v1/parks?parkCode=&stateCode=&q=" + this.state.text + key //fetch the first time to get the park code (unique)
-        fetch(url).then(res => res.json()).then((result) => {
+        fetch(proxy + url).then(res => res.json()).then((result) => {
             this.setState({isLoaded: true, items: result.data})
             const array = this.state.items.filter((item) => item.name === this.state.text)
             const code = array[0].parkCode
             this.setState({parkInfo: array})
             let url_alert = "https://developer.nps.gov/api/v1/alerts?parkCode=" + code + park_key //fetch the second time using the unique park code to get the info about that specific park
-            fetch(url_alert).then(res => res.json()).then((result) => {
+            fetch(proxy + url_alert).then(res => res.json()).then((result) => {
                 this.setState({isAlertLoaded: true, alerts: result.data, parkCode: code})
             }).catch(error => {
                 this.setState({isAlertLoaded: true, error})
